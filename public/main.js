2f4e03d51d0eb899e0ddaae21edc930cb9a96c9c
rachelnicole <loveless@gmail.com>
2018-10-01 19:16:51 -0400

    button press and release switch statement

--- public/main.js
+++ public/main.js
@@ -4,45 +4,53 @@ var m = null;
 navigator.requestMIDIAccess().then(onSuccessCallback, onErrorCallback);
 
 function onSuccessCallback(access) {
-    // If the browser supports WebMIDI, access is a native MIDIAccess
-    // object. If not, it is an instance of a custom class that mimics
-    // the behavior of MIDIAccess using Jazz.
-    m = access;
+  // If the browser supports WebMIDI, access is a native MIDIAccess
+  // object. If not, it is an instance of a custom class that mimics
+  // the behavior of MIDIAccess using Jazz.
+  m = access;
 
-    // Things you can do with the MIDIAccess object:
+  // Things you can do with the MIDIAccess object:
 
-    // inputs = MIDIInputMaps, you can retrieve the inputs with iterators
-    var inputs = m.inputs;
+  // inputs = MIDIInputMaps, you can retrieve the inputs with iterators
+  var inputs = m.inputs;
 
-    // outputs = MIDIOutputMaps, you can retrieve the outputs with iterators
-    var outputs = m.outputs;
+  // outputs = MIDIOutputMaps, you can retrieve the outputs with iterators
+  var outputs = m.outputs;
 
-    // returns an iterator that loops over all inputs
-    var iteratorInputs = inputs.values()
+  // returns an iterator that loops over all inputs
+  var iteratorInputs = inputs.values()
 
-    // get the first input
-    var input = iteratorInputs.next().value
+  // get the first input
+  var input = iteratorInputs.next().value
 
-    // onmidimessage(event), event.data & event.receivedTime are populated
-    input.onmidimessage = myMIDIMessagehandler;
+  // onmidimessage(event), event.data & event.receivedTime are populated
+  input.onmidimessage = myMIDIMessagehandler;
 
-    // returns an iterator that loops over all outputs
-    var iteratorOutputs = outputs.values()
+  // returns an iterator that loops over all outputs
+  var iteratorOutputs = outputs.values()
 
-    // grab first output device
-    var output = iteratorOutputs.next().value;
+  // grab first output device
+  var output = iteratorOutputs.next().value;
 
-    // full velocity note on A4 on channel zero
-    output.send([0x90, 0x45, 0x7f]);
-
-    // full velocity A4 note off in one second.
-    output.send([0x80, 0x45, 0x7f], window.performance.now() + 1000);
+  // full velocity note on A4 on channel zero
+  output.send([0x90, 0x45, 0x7f]);
 };
 
+// this function handles which buttons are being pressed on the midifighter
+
 function myMIDIMessagehandler(iteratorInputs) {
-    console.log(iteratorInputs);
+  switch (iteratorInputs.data[0]) {
+    case 146: // button press
+      console.log('button press. note #' + iteratorInputs.data[1]);
+      break;
+    case 130: // button release
+      console.log('button release. note #' + iteratorInputs.data[1]);
+      break;
+    default:
+      console.log('velocity');
+  }
 
 }
 function onErrorCallback(err) {
-    console.log('uh-oh! Something went wrong! Error code: ' + err.code);
+  console.log('uh-oh! Something went wrong! Error code: ' + err.code);
 }
