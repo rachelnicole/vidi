1de139c4470ea1fd2d0725786b470983b36ce82a
rachelnicole <loveless@gmail.com>
2018-11-20 21:00:24 -0500

    removed old comments

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-// This is how we add in node modules that we want to use in our application.
-// Express is a web application framework, we are going to use it for our simple portfolio site.
 const express = require('express'),
     app = express(),
     path = require('path'),
@@ -8,15 +5,13 @@ const express = require('express'),
     https = require('https'),
     port = process.env.PORT || 3000;
 
-// Telling the app to use the public folder to serve our files is a nice and clean way to manage our site assets
 app.use(express.static(path.join(__dirname, 'public')));
 
 const httpsOptions = {
   key: fs.readFileSync('./key.pem'),
   cert: fs.readFileSync('./cert.pem')
-}
+};
 
-// When the app is run we're going to listen on a port so we know when we have connections from clients
 const server = https.createServer(httpsOptions, app).listen(port, () => {
-  console.log('server running at ' + port)
-})
+  console.log('server running at ' + port);
+}); 
